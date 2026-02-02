cb6e8c88163864e157902c5fcebafeefdd97d262
nabila06 <nabilahcahya066@gmail.com>
2026-02-02 11:34:48 +0800

    mine v5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,28 +13,28 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // Admin User
-        User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
-            'role' => 'admin',
-        ]);
+    // public function run(): void
+    // {
+    //     // Admin User
+    //     User::factory()->create([
+    //         'name' => 'Admin User',
+    //         'email' => 'admin@example.com',
+    //         'password' => bcrypt('password'),
+    //         'role' => 'admin',
+    //     ]);
 
-        // Regular User
-        User::factory()->create([
-            'name' => 'Regular User',
-            'email' => 'user@example.com',
-            'password' => bcrypt('password'),
-            'role' => 'user',
-        ]);
+    //     // Regular User
+    //     User::factory()->create([
+    //         'name' => 'Regular User',
+    //         'email' => 'user@example.com',
+    //         'password' => bcrypt('password'),
+    //         'role' => 'user',
+    //     ]);
 
-        $this->call([
-            CategorySeeder::class,
-            PlaceSeeder::class,
-            KulinerSeeder::class,
-        ]);
-    }
+    //     $this->call([
+    //         CategorySeeder::class,
+    //         PlaceSeeder::class,
+    //         KulinerSeeder::class,
+    //     ]);
+    // }
 }
